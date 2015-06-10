7cf2159956d9b3f102b1ed9ab76f8eac7e483db7
Ankesh Dave <smartyad@gmail.com>
2015-06-10 18:34:41 +0530

    Added New Sample

---
+++ TestTSApp/TestTSApp/app/app.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="../typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+import Zippy = require("app/components/Zippy/zippy");
+@Component({
+    selector: "my-app"
+})
+
+@View({
+        templateUrl: "./app/my-app.html",
+        directives:[Zippy.Zippy]
+})
+
+//my-app Component Controller
+class MyAppComponent {
+    name: string;
+    title:string;
+    constructor(){
+        this.name = "Ankesh";
+        this.title = "Awesome title from Heaven";
+    }
+}
+
+bootstrap(MyAppComponent);
---
+++ TestTSApp/TestTSApp/app/components/Zippy/zippy.ts
@@ -0,0 +1,21 @@
+﻿import {Component, View} from "angular2/angular2"
+
+@Component({
+    selector: "zippy",
+    properties:["title"]
+})
+
+@View({
+    templateUrl: "./app/components/Zippy/zippy.html"
+})
+
+export class Zippy {
+    visible: boolean;
+    constructor(){
+        this.visible = true;
+    }
+
+    toggle(){
+        this.visible = !this.visible;
+    }
+}
